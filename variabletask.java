d515eec550eaf384dbbb591357849a307150ea19
SP20005BSP <145009480+SP20005BSP@users.noreply.github.com>
2025-05-03 11:16:07 +0530

    day2

---
+++ variabletask.java
@@ -0,0 +1,31 @@
+package day2;
+
+public class variabletask {
+public static void main(String args[])
+{
+	int a=3000;
+	byte b=123;
+	short c=12345;
+	long d=900000000;
+	String e="santa";
+	float x=12.12f;
+	double y=12.12365;
+	char z='a';
+	System.out.println("int="+a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	
+	
+	
+	
+			
+			
+}
+	
+
+}
