f78755ad128da14aaa6c98ab8e08c46ae1aea310
SP20005BSP <145009480+SP20005BSP@users.noreply.github.com>
2025-05-30 11:49:24 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,15 @@
+package day8;
+
+public class Employee {
+	
+	int num = 10;
+	public void empDetails() {
+		System.out.println("Employee Name : Vijay");
+	}
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.empDetails();
+	}
+
+}
---
+++ OopsExample.java
@@ -0,0 +1,16 @@
+package day8;
+
+
+public class OopsExample {
+	
+	int num = 10;
+	int num2 = 20;
+	
+	public static void main(String[] args) {
+		
+		OopsExample oops = new OopsExample();
+		
+		System.out.println(oops.num2);
+		
+	}
+}
